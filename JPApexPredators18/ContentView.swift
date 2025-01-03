ae2dd51c3e30427d8fdd9c27ae2ba8a19f266ba7
Mark Santoro <mark_santoro@comcast.net>
2025-01-02 18:40:22 -0800

    Add navigationStack and navigation link to content view. Added search capability with filtering and animation

--- JPApexPredators18/ContentView.swift
+++ JPApexPredators18/ContentView.swift
@@ -10,36 +10,62 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
+    @State var searchText = ""
+    
+    var filteredDinos: [ApexPredator]{
+        if searchText.isEmpty{
+            return predators.apexPredators
+        } else{
+            return predators.apexPredators.filter {
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
     var body: some View {
-        List(predators.apexPredators){ predator in
-            HStack{
+        NavigationStack{
+            List(filteredDinos){ predator in
+                NavigationLink{
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                HStack{
                     //dino image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack(alignment: .leading){
-                        //dino name
-                    Text(predator.name)
-                        .fontWeight(.bold)
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .shadow(color: .white, radius: 1)
                     
-                    //dino type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
+                    VStack(alignment: .leading){
+                        //dino name
+                        Text(predator.name)
+                            .fontWeight(.bold)
+                        
+                        //dino type
+                        Text(predator.type.rawValue.capitalized)
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .padding(.horizontal, 13)
+                            .padding(.vertical, 5)
+                            .background(predator.type.background)
+                            .clipShape(.capsule)
+                    }
                 }
             }
+        }
+        .navigationTitle(Text("Apex Predators"))
+        .searchable(text: $searchText)
+        .autocorrectionDisabled()
+        .animation(.default, value: searchText)
+            
         }
         .preferredColorScheme(.dark)
     }
 }
-
+ 
 #Preview {
     ContentView()
 }
